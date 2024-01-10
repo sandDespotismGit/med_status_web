9fe29966040de1c03226591c2db93b9fc1ce7689
Sand Despotism <wowow3722123@gmail.com>
2024-01-10 23:34:07 +0300

    licenses added

--- src/components/licenses.jsx
+++ src/components/licenses.jsx
@@ -1,32 +1,21 @@
-import { HStack, VStack, Heading, Image } from "@chakra-ui/react";
+import { HStack, VStack, Heading, Image, Stack } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useState } from "react";
+import license1 from "./../images/license1.jpg";
+import license2 from "./../images/license2.jpg";
+import useWindowDimensions from "../hooks/window_dimensions";
 
 const Licenses = () => {
-  const [licenses , setLicenses] = useState([null, null])
-  useEffect(() => {
-    fetch("http://95.214.62.189:1337/api/licenses")
-      .then((response) => response.json())
-      .then(function (commits) {
-        let data = commits.data;
-        let buffer = [];
-        for (let elem of data) {
-          buffer.push(
-            
-          );
-        }
-        setLicenses(buffer);
-      });
-  }, []);
+  const {width, height} = useWindowDimensions();
   return (
     <VStack width="100%" spacing="25px">
       <Heading size="lg" color="#085D65">
         Лицензии
       </Heading>
-      <HStack justify="space-around" width='30%' spacing={"100px"}>
-        <Image  />
-        <Image  />
-      </HStack>
+      <Stack justify="center" align={'center'} width="95%" spacing={"10px"} direction={width >= 820 ? 'row' : 'column'}>
+        <Image src={license1} width={width >= 820 ? '45%' : '95%'}/>
+        <Image src={license2} width={width >= 820 ? '45%' : '95%'}/>
+      </Stack>
     </VStack>
   );
 };
